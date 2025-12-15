0663e3a07c727d1507aac230eb279169d5510005
ricomateo <mrico@fi.uba.ar>
2025-12-15 16:39:38 -0300

    add burn and burnFrom functions

--- src/Stablecoin.sol
+++ src/Stablecoin.sol
@@ -18,6 +18,7 @@ contract Stablecoin is
 {
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
 
     mapping(address => uint256) public minterAllowance;
@@ -26,13 +27,15 @@ contract Stablecoin is
         _disableInitializers();
     }
 
-    function initialize(string memory name, string memory symbol) public initializer {
+    function initialize(string memory name, string memory symbol, address admin, address burner) public initializer {
         __ERC20_init(name, symbol);
         __ERC20Burnable_init();
         __AccessControl_init();
 
         _setRoleAdmin(MINTER_ROLE, ADMIN_ROLE);
-        _grantRole(ADMIN_ROLE, msg.sender);
+        _setRoleAdmin(BURNER_ROLE, ADMIN_ROLE);
+        _grantRole(ADMIN_ROLE, admin);
+        _grantRole(BURNER_ROLE, burner);
     }
 
     function addMinter(address newMinter, uint256 amount) public onlyRole(ADMIN_ROLE) {
@@ -40,10 +43,23 @@ contract Stablecoin is
         grantRole(MINTER_ROLE, newMinter);
     }
 
+    function addBurner(address newBurner) public onlyRole(ADMIN_ROLE) {
+        grantRole(BURNER_ROLE, newBurner);
+    }
+
     function mint(address to, uint256 value) public onlyRole(MINTER_ROLE) {
         require(minterAllowance[msg.sender] >= value, "Value exceeds allowance");
         minterAllowance[msg.sender] -= value;
         _mint(to, value);
+    }
+
+    function burn(uint256 value) public override onlyRole(BURNER_ROLE) {
+        _burn(_msgSender(), value);
+    }
+
+    function burnFrom(address account, uint256 value) public override onlyRole(BURNER_ROLE) {
+        _spendAllowance(account, _msgSender(), value);
+        _burn(account, value);
     }
 
     function _authorizeUpgrade(address newImplementation) internal override onlyRole(ADMIN_ROLE) {}
--- test/Stablecoin.t.sol
+++ test/Stablecoin.t.sol
@@ -9,13 +9,17 @@ import {ERC1967Proxy} from
 contract StablecoinTest is Test {
     Stablecoin public stablecoin;
     address public admin = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+    address public minter = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
+    address public burner = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC;
 
     function setUp() public {
         vm.startPrank(admin);
         address impl = address(new Stablecoin());
-        address proxy =
-            address(new ERC1967Proxy(impl, abi.encodeCall(Stablecoin.initialize, ("Stablecoin", "Stablecoin"))));
+        address proxy = address(
+            new ERC1967Proxy(impl, abi.encodeCall(Stablecoin.initialize, ("Stablecoin", "Stablecoin", admin, burner)))
+        );
         stablecoin = Stablecoin(proxy);
+        stablecoin.addMinter(minter, 1000);
         vm.stopPrank();
     }
 
@@ -51,5 +55,48 @@ contract StablecoinTest is Test {
         uint256 amount = 1000;
         vm.expectRevert();
         stablecoin.mint(nonMinter, amount);
+    }
+
+    // TODO: test edge cases
+    function test_Burn() public {
+        uint256 amount = 100;
+
+        // Mint tokens to the burner
+        vm.prank(minter);
+        stablecoin.mint(burner, amount);
+        uint256 burnerBalance = stablecoin.balanceOf(burner);
+
+        // Burn the burner tokens
+        vm.prank(burner);
+        stablecoin.burn(amount);
+
+        uint256 expectedBalance = burnerBalance - amount;
+        assertEq(stablecoin.balanceOf(burner), expectedBalance);
+    }
+
+    function test_BurnFrom() public {
+        address account = address(3);
+        uint256 amount = 100;
+
+        // Mint tokens to the account
+        vm.prank(minter);
+        stablecoin.mint(account, amount);
+        uint256 initialAccountBalance = stablecoin.balanceOf(account);
+
+        // Approve allowance to the burner from the account
+        vm.prank(account);
+        stablecoin.approve(burner, amount);
+
+        vm.prank(burner);
+        stablecoin.burnFrom(account, amount);
+        assertEq(stablecoin.balanceOf(burner), initialAccountBalance - amount);
+    }
+
+    function test_NonBurnerAccountCannotBurn() public {
+        address nonBurnerAccount = address(3);
+        uint256 amount = 1000;
+        vm.prank(nonBurnerAccount);
+        vm.expectRevert();
+        stablecoin.burn(amount);
     }
 }
