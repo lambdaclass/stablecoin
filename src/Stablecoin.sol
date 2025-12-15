43fe024a26e2ee1ae00325faf30d9d0768cb146c
ricomateo <mrico@fi.uba.ar>
2025-12-15 13:17:49 -0300

    add contract scaffolding with some roles

---
+++ src/Stablecoin.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: UNLICENSED
+// TODO: add the right license
+pragma solidity ^0.8.13;
+
+import {ERC20BurnableUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract Stablecoin is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, AccessControlUpgradeable, UUPSUpgradeable {
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(string memory name, string memory symbol) initializer public {
+        __ERC20_init(name, symbol);
+        __ERC20Burnable_init();
+        __AccessControl_init();
+
+        _setRoleAdmin(MINTER_ROLE, ADMIN_ROLE);
+        _grantRole(ADMIN_ROLE, msg.sender);
+    }
+
+    function addMinter(address newMinter) public onlyRole(ADMIN_ROLE) {
+        grantRole(MINTER_ROLE, newMinter);
+    }
+
+    function mint(address to, uint256 value) public onlyRole(MINTER_ROLE) {
+        _mint(to, value);
+    }
+    
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        override
+        onlyRole(ADMIN_ROLE)
+    {}
+}
