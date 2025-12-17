6ac69485f592b0a04958c855f9a7077d3f7f5c3f
ricomateo <mrico@fi.uba.ar>
2025-12-17 12:49:35 -0300

    improve deployment script

--- script/DeployStablecoin.s.sol
+++ script/DeployStablecoin.s.sol
@@ -6,18 +6,19 @@ import {Stablecoin} from "src/Stablecoin.sol";
 import {Upgrades} from "@openzeppelin-foundry-upgrades/Upgrades.sol";
 
 contract DeployStablecoin is Script {
-    function run(
-        string memory name,
-        string memory symbol,
-        address admin,
-        address burner,
-        address pauser,
-        address freezer
-    ) public {
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        string memory name = vm.envString("STABLECOIN_NAME");
+        string memory symbol = vm.envString("STABLECOIN_SYMBOL");
+        address admin = vm.envAddress("ADMIN_ADDRESS");
+        address burner = vm.envAddress("BURNER_ADDRESS");
+        address pauser = vm.envAddress("PAUSER_ADDRESS");
+        address freezer = vm.envAddress("FREEZER_ADDRESS");
+
         bytes memory initializerData =
             abi.encodeCall(Stablecoin.initialize, (name, symbol, admin, burner, pauser, freezer));
-
-        vm.startBroadcast();
+        vm.startBroadcast(deployerPrivateKey);
         address stablecoin = Upgrades.deployUUPSProxy("Stablecoin.sol", initializerData);
         vm.stopBroadcast();
 
