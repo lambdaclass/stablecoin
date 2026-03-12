f38dfe9329b3e27c59b46ec3b8cefe752327dcc0
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2026-03-12 12:53:16 -0300

    refactor: remove unused imports

--- src/bridge/BridgeBurner.sol
+++ src/bridge/BridgeBurner.sol
@@ -4,7 +4,6 @@ pragma solidity =0.8.30;
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IBridgeBurner} from "./interfaces/IBridgeBurner.sol";
 import {IOutbox} from "./interfaces/IOutbox.sol";
 import {Stablecoin} from "../Stablecoin.sol";
